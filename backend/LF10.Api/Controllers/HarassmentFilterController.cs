4c89953d860628d570a5c4a5bf53255715717d76
Luca <109668211+Luca148@users.noreply.github.com>
2026-09-02 11:31:15 +0200

    basic filter

---
+++ backend/LF10.Api/Controllers/HarassmentFilterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LF10.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HarassmentFilterController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Post([FromBody] string value)
+    {
+        if (value.Contains("hate")) value = "love";
+
+        return Ok(value);
+    }
+}
